c1514f7cc6d2ab510338da9bc1b0a7de9f0eb219
emanuel.vargas <emanuel.vargas@globant.com>
2018-08-28 14:10:01 -0300

    Clase Cuente agregada

--- src/main/java/com/pruebagit/ejemplo_git/models/cuenta.java
+++ src/main/java/com/pruebagit/ejemplo_git/models/cuenta.java
@@ -1,4 +1,33 @@
 package com.pruebagit.ejemplo_git.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="cuenta")
 public class cuenta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idCuenta;
+    private String tipo;
+
+    public cuenta(int idCuenta, String tipo) {
+        this.idCuenta = idCuenta;
+        this.tipo = tipo;
+    }
+
+    public int getIdCuenta() {
+        return idCuenta;
+    }
+
+    public void setIdCuenta(int idCuenta) {
+        this.idCuenta = idCuenta;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
 }
