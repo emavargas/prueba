288b41d16c06069cf6c7f750907a45815ae85220
emanuel.vargas <emanuel.vargas@globant.com>
2018-08-28 09:57:44 -0300

    userByid Added

--- src/main/java/com/pruebagit/ejemplo_git/controllers/controllerUsers.java
+++ src/main/java/com/pruebagit/ejemplo_git/controllers/controllerUsers.java
@@ -2,6 +2,7 @@ package com.pruebagit.ejemplo_git.controllers;
 
 import com.pruebagit.ejemplo_git.models.users;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -12,5 +13,10 @@ public class controllerUsers {
     @GetMapping("/users")
     public String getUsers(){
         return "todos los usuarios";
+    }
+
+    @GetMapping("users/{id}")
+    public String getUserById(@PathVariable int id){
+        return "Un usuario especifico";
     }
 }
